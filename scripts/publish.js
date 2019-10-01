f0f0bee0db6829a81a27baa2c6ab60ccb1d04b25
Jonny Hork <jhork@salesforce.com>
2019-10-01 14:47:25 -0600

    use personal organization for testing"

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,7 +5,7 @@ shell.set('-e');
 shell.set('+v');
 
 // This needs to change when we are ready to publish
-const DOCKER_HUB_REPOSITORY = 'salesforce/salesforcedx';
+const DOCKER_HUB_REPOSITORY = 'therealjonnyhork/salesforcedx';
 
 // Checks that you have the Docker CLI installed
 if (!shell.which('docker')) {
